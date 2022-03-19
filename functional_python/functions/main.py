6372681eb9d6f31dd2167cdea51dab25691e092a
David Vujic <david@vujic.se>
2022-03-19 14:41:05 +0100

    add startin poing example code in functions section

--- functional_python/functions/main.py
+++ functional_python/functions/main.py
@@ -1,2 +1,9 @@
-def comment():
-    pass
+words = ["hello", "pythonic", "world"]
+
+
+def find_word(word: str, words: list) -> str | None:
+    for w in words:
+        if w == word:
+            return w
+
+    return None
